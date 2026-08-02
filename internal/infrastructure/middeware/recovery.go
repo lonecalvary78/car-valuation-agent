80ccbd61d03a8065d8d8e71a7c54bd764deac172
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2026-08-03 03:34:23 +0800

    Fix the recovery middleware based on the code review

--- internal/infrastructure/middeware/recovery.go
+++ internal/infrastructure/middeware/recovery.go
@@ -7,11 +7,13 @@ import (
 
 func Recovery(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := recover()
-		if err != nil {
-			error := fmt.Errorf("%v", err)
-			http.Error(w, error.Error(), http.StatusInternalServerError)
-		}
+		defer func() {
+			err := recover()
+			if err != nil {
+				error := fmt.Errorf("%v", err)
+				http.Error(w, error.Error(), http.StatusInternalServerError)
+			}
+		}()
 		next.ServeHTTP(w, r)
 	})
 }
