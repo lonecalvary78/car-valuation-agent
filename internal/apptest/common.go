6c99ffce36ba309c4660623927e69d2c1a881281
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2026-08-09 13:03:06 +0800

    Located common.go into apptest package to make it resuable for cross package testing

--- internal/apptest/common.go
+++ internal/apptest/common.go
@@ -1,11 +1,13 @@
-package config
+package apptest
 
 import "testing"
 
-func SetEnv(t *testing.T) {
+func SetEnvForTesting(t *testing.T) {
 	t.Helper()
 	t.Setenv("SERVER_HOST", "localhost")
 	t.Setenv("SERVER_PORT", "8585")
+	t.Setenv("SERVER_READ_TIMEOUT", "30s")
+	t.Setenv("SERVER_WRITE_TIMEOUT", "2m")
 	t.Setenv("BASE_URL", "http://localhost:8000")
 	t.Setenv("API_KEY", "apikey")
 	t.Setenv("AGENT_NAME", "car-valuation-agent")
@@ -16,4 +18,6 @@ func SetEnv(t *testing.T) {
 	t.Setenv("PRESEMCE_PENALTY", "1.0")
 	t.Setenv("FREQENCY_PENALTY", "1.0")
 	t.Setenv("SKILL_LOCATION", "skills")
+	t.Setenv("WAIT_TIMEOUT", "60s")
+
 }
